a67538101f47bfddf5f1c6c9d8b323421b813a20
emilym <emily.markova@gmail.com>
2024-10-28 23:44:53 -0700

    Make footer a component and put images into a folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import "./App.css";
 import Button from "@mui/joy/Button";
-import arrow from "./arrow.png";
-import insta_logo from "./insta_logo.png";
-import { keyframes } from "@mui/system";
+import arrow from "./images/arrow.png";
+import Footer from "./components/Footer";
 
-const glow = keyframes`
-  0% {
-    box-shadow: 0 0 3px rgba(138, 43, 226, 0.3), 0 0 6px rgba(138, 43, 226, 0.3);
-  }
-  50% {
-    box-shadow: 0 0 8px rgba(138, 43, 2265, 0.5), 0 0 12px rgba(138, 43, 226, 0.5);
-  }
-  100% {
-    box-shadow: 0 0 3px rgba(138, 43, 226, 0.3), 0 0 6px rgba(138, 43, 226, 0.3);
-  }
-`;
 function App() {
   return (
     <div>
@@ -35,31 +23,7 @@ function App() {
       </div>
       <br />
       <br />
-      <div id="footer">
-        <Button
-          sx={{ animation: `${glow} 2s infinite alternate` }}
-          id="devpost"
-          component="a"
-          href="https://mvhacks-6-0.devpost.com/project-gallery"
-        >
-          Previous Projects
-        </Button>
-        <br />
-        <br />
-        <Button
-          sx={{ animation: `${glow} 2s infinite alternate` }}
-          id="contactUs"
-          component="a"
-          href="mailto:alexm@mvhacks.io"
-        >
-          Contact Us
-        </Button>
-        <br />
-        <br />
-        <a id="instagram" href="https://www.instagram.com/mvhacks/">
-          <img src={insta_logo} width={50} height={50} />
-        </a>
-      </div>
+      <Footer></Footer>
     </div>
   );
 }
