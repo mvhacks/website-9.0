cc45dcd6b5c96165d003139e48e0d82d22b84d4c
FordoA455 <milobylin@gmail.com>
2024-11-02 23:51:57 -0700

    Adding nav bar scroll functionality
    Signed-off-by: FordoA455 <milobylin@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import "../App.css";
 import Link from '@mui/joy/Link';
 
-export default function Navbar() {
+export default function Navbar({ onOptionSelect }: any) {
   return (
     <div>
-      <Link component="button" color="neutral" level="h2" underline="none" variant="plain">HOME</Link> 
-      <Link component="button" color="neutral" level="h2" underline="none" variant="plain">APPLY</Link> 
-      <Link component="button" color="neutral" level="h2" underline="none" variant="plain">FAQS</Link> 
-      <Link component="button" color="neutral" level="h2" underline="none" variant="plain">SPONSOR</Link> 
-      <Link component="button" color="neutral" level="h2" underline="none" variant="plain">TEAM</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain"  onClick = {() => onOptionSelect('home')}>HOME</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain" >APPLY</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain" onClick = {() => onOptionSelect('info')}>INFO</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain" onClick = {() => onOptionSelect('schedule')}>SCHEDULE</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain" onClick = {() => onOptionSelect('sponsor')}>SPONSOR</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain" onClick = {() => onOptionSelect('faqs')}>FAQS</Link> 
+      <Link component="button" color="neutral" level="h2" underline="none" variant="plain" onClick = {() => onOptionSelect('team')}>TEAM</Link> 
     </div>
   );
 }
