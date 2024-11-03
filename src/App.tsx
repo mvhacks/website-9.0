26cadb9454ad28c2f60c8fdd0a5b0fe5af504fb8
FordoA455 <milobylin@gmail.com>
2024-11-02 23:51:57 -0700

    Adding nav bar scroll functionality
    Signed-off-by: FordoA455 <milobylin@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import react, { useState, useRef } from 'react';
 import "./App.css";
 import Button from "@mui/joy/Button";
 import arrow from "./images/arrow.png";
@@ -5,14 +6,64 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  const [selectedScrollOption, setSelectedScrollOption] = useState("");
+  const home = useRef(null);
+  const info = useRef(null);
+  const schedule = useRef(null);
+  const sponsor = useRef(null);
+  const faqs = useRef(null);
+  const team = useRef(null);
+
+  const scroll = (elementRef: any) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: 'smooth'
+    });
+  };
+
+  const handleOptionChange = (option: any) => {
+    setSelectedScrollOption(option);
+    if (option === 'home') {
+      scroll(home);
+    } else if (option === "info") {
+      scroll(info);
+    } else if (option === "schedule") {
+      scroll(schedule);
+    } else if (option === "sponsor") {
+      scroll(sponsor);
+    } else if (option === "faqs") {
+      scroll(faqs);
+    } else if (option === "team") {
+      scroll(team);
+    }
+  };
+
   return (
     <div>
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
       </style>
-      <Navbar />
+      <Navbar onOptionSelect={handleOptionChange}/>
       <div>
+      <div className="home" ref={home}>
+        <h2>HOME</h2>
+      </div>
+      <div className="info" ref={info}>
+        <h2>INFO</h2>
+      </div>
+      <div className="schedule" ref={schedule}>
+        <h2>SCHEDULE</h2>
+      </div>
+      <div className="sponsor" ref={sponsor}>
+        <h2>SPONSOR</h2>
+      </div>
+      <div className="faqs" ref={faqs}>
+        <h2>FAQS</h2>
+      </div>
+      <div className="team" ref={team}>
+        <h2>TEAM</h2>
+      </div>
       <Button
         sx={{ borderRadius: "25px" }}
         id="alert"
