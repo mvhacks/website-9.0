da785dfbf1b72b87f0a62a75352fe6affa7e5a82
Noam Kassoff <noamkassoff@gmail.com>
2024-11-16 22:59:41 -0800

    Added placeholder blurb and prospectus image

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Button from "@mui/joy/Button";
 import arrow from "./images/arrow.png";
 import Footer from "./components/Footer";
+import Prospectus from "./images/prospectus.png"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
       </style>
       <div>
+      <div id="blurbSponsors"><h1>Sponsor MVHacks 7.0!</h1><p>MVHacks is now in its 8th year of inspiring high school students throughout the country to get together and build great projects. As a prestigious, in-person hackathon, our past events have consistently attracted over 200 unique and diverse attendees. With your support, we aim to engage over 250+ students for MVHacks.</p><p>To discover more about sponsorship opportunities, please download our Sponsorship Prospectus below and reach out to us at <a id="email" href="mailto:hello@mvhacks.io">hello@mvhacks.io</a>.</p><p>Join us, and let's make MVHacks 8.0 an unforgettable experience. We eagerly await the participation of your company!</p><a id="downloadProspectus" href="about:blank">Download the Sponsorship Prospectus</a></div>
+      <img src={Prospectus} id="prospectus" width="80%" height="auto"/>
       <Button
         sx={{ borderRadius: "25px" }}
         id="alert"
