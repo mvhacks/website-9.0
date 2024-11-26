ab076cba2eb2e29fac41bc4ef500bcf450f18149
noamkassoff <69400064+noamkassoff@users.noreply.github.com>
2024-11-26 14:40:31 -0800

    Fixed sponsors formatting

--- src/components/Prospectus.tsx
+++ src/components/Prospectus.tsx
@@ -1,8 +1,19 @@
 import prospectusImage from "../images/prospectus.png";
+import Typography from '@mui/joy/Typography';
 export default function Prospectus() {
     return (<div>
         <div id="blurbSponsors">
-            <h1>Sponsor MVHacks 8.0!</h1>
+            <Typography level='h2'
+                sx={{ 
+                    color: '#ffffff', 
+                    marginBottom: '2%', 
+                    marginRight: '0.7%',
+                    marginLeft: '0.7%',
+                    textAlign: "center" 
+                }}
+            >
+                SPONSOR MVHACKS 8.0!
+            </Typography>
             <p>MVHacks is now in its 8th year of inspiring high school students throughout the country to get together and build great projects. As a prestigious, in-person hackathon, our past events have consistently attracted over 200 unique and diverse attendees. With your support, we aim to engage over 250+ students for MVHacks.</p>
             <p>To discover more about sponsorship opportunities, please download our Sponsorship Prospectus below and reach out to us at&nbsp;
                 <a id="email" href="mailto:hello@mvhacks.io">hello@mvhacks.io</a>.</p>
