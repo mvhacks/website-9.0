fd6ce3126ae03018b520de3b52fe036d30fcfd03
FordoA455 <milobylin@gmail.com>
2024-11-26 16:31:45 -0800

    Adding info page file
    Signed-off-by: FordoA455 <milobylin@gmail.com>

---
+++ src/components/InfoPage.tsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import Grid from '@mui/joy/Grid';
+import React, { useState, useEffect } from 'react';
+
+export default function InfoPage() {
+  return (
+    <Sheet>
+      <Grid container spacing={2} sx={{flexGrow: 1}}>
+
+      </Grid>
+    </Sheet>);
+}
