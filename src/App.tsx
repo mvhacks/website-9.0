64c6e8a7e011a4f246532461f6b5c6ad88319633
exersonz <cinty.lin.cinty@gmail.com>
2026-01-20 20:45:43 -0800

    Added sponsors

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Schedule from "./components/Schedule";
 import InfoPage from './components/InfoPage';
 import Carousel from "./components/Carousel";
 import Team from "./components/Team";
+import Sponsors from "./components/Sponsors";
 import BubbleCursor from './components/BubbleCursor';
 import LiquidFilter from './LiquidGlassFilter';
 import FloatingSubmarine from "./components/FloatingSubmarine";
@@ -20,6 +21,7 @@ function App() {
   const sponsor = useRef(null);
   const faqs = useRef(null);
   const team = useRef(null);
+  const sponsors = useRef(null);
 
   const scroll = (elementRef: any) => {
     window.scrollTo({
@@ -55,24 +57,27 @@ function App() {
           @import
           url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
         </style>
-        <div className="home" ref={home}>
+        <div ref={home}>
           <Title></Title>
         </div>
-        <div className="info" ref={info}>
+        <div ref={info}>
           <InfoPage></InfoPage>
           <Carousel></Carousel>
         </div>
-        <div className="schedule" ref={schedule}>
+        <div ref={schedule}>
           <Schedule></Schedule>
         </div>
-        <div className="faqs" ref={faqs}>
+        <div ref={faqs}>
           <Faqs></Faqs>
         </div>
-        <div className="sponsor" ref={sponsor}>
+        <div ref={sponsor}>
             <Prospectus></Prospectus>
         </div>
-        <div className="team" ref={team}>
+        <div ref={team}>
           <Team></Team>
+        </div>
+        <div ref={sponsors}>
+          <Sponsors></Sponsors>
         </div>
       </div>
       <br />
